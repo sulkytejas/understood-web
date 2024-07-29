a87b5cbab5451afb6273f2e7a71a9e8013702553
Tejas Pashte <tejas@Tejass-Mac-Studio.local>
2024-07-29 18:12:05 +0530

    chnaged back url

--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -126,7 +126,7 @@ const VideoCall = () => {
 
     const initializeSocket = () => {
         // socket.current = io('https://translations-1153aabe3d6b.herokuapp.com');
-        socket.current = io('https://3.25.64.73/socket');
+        socket.current = io('https://3.25.64.73/socket.io');
 
         socket.current.on('connect', () => {
             setConnected(true);
