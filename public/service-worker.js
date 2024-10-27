779de8229490a7b0842c93f5677b0822ad11003b
Tejas Pashte <tejas@Tejass-Mac-Studio.local>
2024-10-27 18:03:06 +0530

    testing workbox

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,10 +8,23 @@ if (workbox) {
   // Your caching logic will go here
 
   // Cache requests to tfhub.dev
+  workbox.routing.registerRoute(
+    ({ url }) => url.origin === 'https://tfhub.dev',
+    new workbox.strategies.CacheFirst({
+      cacheName: 'tfhub-model-cache',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for one week
+        }),
+      ],
+    }),
+  );
+
+  //   // Cache requests to storage.googleapis.com
   //   workbox.routing.registerRoute(
-  //     ({ url }) => url.origin === 'https://tfhub.dev',
+  //     ({ url }) => url.origin === 'https://storage.googleapis.com',
   //     new workbox.strategies.CacheFirst({
-  //       cacheName: 'tfhub-model-cache',
+  //       cacheName: 'gcs-model-cache',
   //       plugins: [
   //         new workbox.expiration.ExpirationPlugin({
   //           maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for one week
@@ -20,36 +33,23 @@ if (workbox) {
   //     }),
   //   );
 
-  //   //   // Cache requests to storage.googleapis.com
-  //   //   workbox.routing.registerRoute(
-  //   //     ({ url }) => url.origin === 'https://storage.googleapis.com',
-  //   //     new workbox.strategies.CacheFirst({
-  //   //       cacheName: 'gcs-model-cache',
-  //   //       plugins: [
-  //   //         new workbox.expiration.ExpirationPlugin({
-  //   //           maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for one week
-  //   //         }),
-  //   //       ],
-  //   //     }),
-  //   //   );
+  // Configure Service Worker for Offline Support
+  workbox.routing.registerRoute(
+    ({ request }) =>
+      request.destination === 'script' ||
+      request.destination === 'style' ||
+      request.destination === 'image',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-resources',
+    }),
+  );
 
-  //   // Configure Service Worker for Offline Support
-  //   workbox.routing.registerRoute(
-  //     ({ request }) =>
-  //       request.destination === 'script' ||
-  //       request.destination === 'style' ||
-  //       request.destination === 'image',
-  //     new workbox.strategies.StaleWhileRevalidate({
-  //       cacheName: 'static-resources',
-  //     }),
-  //   );
-
-  //   // Handle Navigation Requests:
-  //   workbox.routing.registerRoute(
-  //     new workbox.routing.NavigationRoute(
-  //       workbox.precaching.createHandlerBoundToURL('/index.html'),
-  //     ),
-  //   );
+  // Handle Navigation Requests:
+  workbox.routing.registerRoute(
+    new workbox.routing.NavigationRoute(
+      workbox.precaching.createHandlerBoundToURL('/index.html'),
+    ),
+  );
 } else {
   console.log(`Workbox didn't load`);
 }
