28b09e0222453706ddf9f5ccef4b59737d8dd0dc
Tejas Pashte <tejas@Tejass-Mac-Studio.local>
2024-12-16 21:32:05 +0530

    added marathi language

--- src/components/Login/firebase.js
+++ src/components/Login/firebase.js
@@ -6,24 +6,14 @@ import {
   browserLocalPersistence,
 } from 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyAjsXNAKhIIR3c4ZHIvQpDXIIJLNkeJOis',
-  authDomain: 'understood-f356b.firebaseapp.com',
-  projectId: 'understood-f356b',
-  storageBucket: 'understood-f356b.firebasestorage.app',
-  messagingSenderId: '114523086382',
-  appId: '1:114523086382:web:df11f80296013cd74509bf',
-  measurementId: 'G-DGRTQ32K18',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
